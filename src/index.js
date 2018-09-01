f3db5f4629998e6862cc6a577f2595bfc7225974
Jameel Bokhari (macbook) <jameelbokhari@gmail.com>
2018-09-01 09:07:37 -0700

    Clean up step - link https://webpack.js.org/guides/asset-management/#wrapping-up

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import _ from 'lodash'; 
-import './style.css';
-import Icon from './icon.png';
-import data from './data.json';
-import xmlData from './data.xml';
 
 function component() {
 	let element = document.createElement("div");
 
-	element.innerHTML = _.join(data.text, " ");
+	element.innerHTML = _.join(["Hello", "webpack"], " ");
 	element.classList.add('hello');
-
-	var myIcon = new Image();
-	myIcon.src = Icon;
-
-	element.appendChild(myIcon);
-
-	console.log(xmlData);
 
 	return element;
 }
