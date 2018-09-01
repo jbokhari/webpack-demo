7584099ae8491bfe1078e62373b6bb07efa11906
Jameel Bokhari (macbook) <jameelbokhari@gmail.com>
2018-09-01 08:57:22 -0700

    Loading json (not a tutorial step)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import _ from 'lodash'; 
 import './style.css';
 import Icon from './icon.png';
+import data from './data.json';
 
 function component() {
 	let element = document.createElement("div");
 
-	element.innerHTML = _.join(["Hello", "webpack"], " ");
+	element.innerHTML = _.join(data.text, " ");
 	element.classList.add('hello');
 
 	var myIcon = new Image();
